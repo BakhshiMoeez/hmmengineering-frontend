e05f43f245231c1b7f8f488b8ccc98fa514aa25d
BakhshiMoeez <moeezbakhshi123456@gmail.com>
2024-02-04 20:35:11 +0500

    Adding _redirects file, adding herosection

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,9 +1,26 @@
+import Container from 'react-bootstrap/Container';  
 import './HeroSection.css';
 
 export default function HeroSection() {
   return (
     <>
-        
+      <div className="hero-section-main-container">
+        <div className="hero-section-main-container-image">
+          <img src="/assets/Heropage/heropagepic.jpg" alt="hero-page-main-pic" />
+        </div>
+        <div className='hero-section-main-container-image-cover'>
+          <Container>
+              <div className="hero-section-main-container-main-content-section">
+                <div className="hero-section-main-container-main-content-section-title">
+                  <h1>Welcome to HmmEngineering</h1>
+                </div>
+                <div className="hero-section-main-container-main-content-section-description">
+                  <p>We provide you with Fancy, Luxurious and Affordable Street poles brimming with elegance.</p>
+                </div>
+              </div>
+          </Container>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,10 @@
+import HeroSection from '../heroSection/HeroSection';
 import './Home.css';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <HeroSection />
+    </>
   )
 }
