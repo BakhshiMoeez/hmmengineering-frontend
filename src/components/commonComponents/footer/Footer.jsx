e11283859dfebef7830f90446be80f3b1018ae3c
BakhshiMoeez <moeezbakhshi123456@gmail.com>
2024-02-05 14:31:30 +0500

    Adding footer

--- src/components/commonComponents/footer/Footer.jsx
+++ src/components/commonComponents/footer/Footer.jsx
@@ -1,7 +1,70 @@
+import Container from 'react-bootstrap/Container';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className="footer-main-container">
+        <Container>
+          <div className="footer-sub-main-container">
+            <div className="footer-sub-main-left-section">
+              <h1 className='footer-sub-main-left-section-title'>HmmEngineering</h1>
+              <p className='footer-sub-main-left-section-content'>Fancy and Luxurious street poles</p>
+              <div className="footer-left-section-social-media-icons">
+                <span className="footer-social-media-icon">
+                  <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FacebookIcon /></a>
+                </span>
+                <span className="footer-social-media-icon">
+                  <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><InstagramIcon /></a>
+                </span>
+                <span className="footer-social-media-icon">
+                  <a href="https://www.youtube.com/" target="_blank" rel="noreferrer"><YouTubeIcon /></a>
+                </span>
+              </div>
+            </div>
+            <div className="footer-sub-main-right-section">
+              <div className="footer-right-section-content-main-container">
+                <div className="footer-right-section-content-sub-main-container">
+                  
+                  <div className="footer-right-section-content-sub-main-container-item">
+                    <span className='footer-right-content-item-icon'>
+                      <LocationOnIcon style={{color: 'white'}}/>
+                    </span>
+                    <span className='footer-right-content-item-content'>
+                      <p>Do moriya pul</p>
+                    </span>
+                  </div>
+
+                  <div className="footer-right-section-content-sub-main-container-item">
+                    <span className='footer-right-content-item-icon'>
+                      <LocalPhoneIcon style={{color: 'white'}}/>
+                    </span>
+                    <span className='footer-right-content-item-content'>
+                      <p>0321-1234567</p>
+                    </span>
+                  </div>
+
+                  <div className="footer-right-section-content-sub-main-container-item">
+                    <span className='footer-right-content-item-icon'>
+                      <WhatsAppIcon style={{color: 'white'}}/>
+                    </span>
+                    <span className='footer-right-content-item-content'>
+                      <p>0321-1234567</p>
+                    </span>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
   )
 }
