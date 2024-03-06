9c7bf6e5849314086977dc27913d5276fcb2077b
BakhshiMoeez <moeezbakhshi123456@gmail.com>
2024-03-06 21:39:55 +0500

    Making About us responsive

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -22,22 +22,24 @@ export default function AboutUs() {
 
   return (
     <div className='aboutus-main-container'>
-      <Container>
       <div className="about-us-hero-section" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0)), url(${images[currentImageIndex]})` }}>
-      <div className="about-us-hero-section-content" >
-        <h1 className='about-us-hero-section-heading' style={{color:"#FEFEFA"}}>About Us</h1>
-        <p className='about-us-hero-section-paragraph' style={{color:"#FEFEFA", fontSize:"1.2rem"}}>Driven by a vision of prosperity and progress, Pakistani Iron &
-          Steel Mill continues to be a driving force in shaping the nation's industrial landscape. With a steadfast focus
-          on innovation, efficiency, and customer satisfaction, we remain dedicated to forging a brighter future for Pakistan
-          and beyond. Join us on our journey as we continue to set new benchmarks in the steel industry, forging partnerships,
-          and building bridges towards a stronger, more resilient tomorrow.</p>
+        <div className="about-us-hero-section-content" >
+          <Container>
+          <div className="about-us-hero-section-content-inner-wrapper">
+            <h1 className='about-us-hero-section-heading'>About Us</h1>
+            <p className='about-us-hero-section-paragraph'>
+              Driven by a vision of prosperity and progress, Pakistani Iron & Steel Mill continues to be a driving force in shaping the nation's industrial landscape. With a steadfast focus
+              on innovation, efficiency, and customer satisfaction, we remain dedicated to forging a brighter future for Pakistan
+              and beyond. Join us on our journey as we continue to set new benchmarks in the steel industry, forging partnerships,
+              and building bridges towards a stronger, more resilient tomorrow.</p>
           </div>
-        
+          </Container>
+        </div>
       </div>
-      
-      <div className="about-us-content-section" style={{marginTop:"2rem"}}>
-        <h2>Our Company</h2>
-        <p>
+      <Container>
+      <div className="about-us-content-section">
+        <h2 className='about-us-content-section-heading-common'>Our Company</h2>
+        <p className='about-us-content-section-paragraph-common'>
           Our steel manufacturing venture began back in 1970 from steel trading in Lahore.
           However, it was not until 1978 that we began manufacturing steel in Islamabad with a capacity of 38,000 – 40,000 TPA.
           Our growth has been proportional with that of the needs of the industry and the local market.
@@ -49,55 +51,65 @@ export default function AboutUs() {
           bear the majority of the electrical needs of the manufacturing plants.
         </p>
 
-        <h2 style={{marginTop:"2rem"}}>Our Products</h2>
-        <p>
+        <h2 className='about-us-content-section-heading-common'>Our Products</h2>
+        <p className='about-us-content-section-paragraph-common'>
           Hmm Steel currently manufactures Grade 40 and Grade 60 products that are a fine depiction of durability, strength,
           and life-long investment. Following the international quality standards while manufacturing our products has
           guaranteed our sales all across Pakistan. Whether it is the construction of a state-of-the-art, high rise monuments,
           or related infrastructure, our products will fit your needs.
         </p>
 
-        <h1 style={{marginBottom:"3rem", marginTop:"2rem"}}>Largest Billet Manufacturer In Pakistan</h1>
-
-        <h2 className='about-us-staff-heading'>
-          Our Staff
-        </h2>
-        <div className="about-us-image-container">
-          <img src={staffPics[0]} alt="chairman pic here" style={{ width: "20rem" }} />
-          <strong>Sheikh Abdullah Nazim</strong>
-          <small> Founder/Chairman</small>
+        <div className="about-us-staff-intro-section">
+          <div className="about-us-staff-heading-container">
+            <h2 className='about-us-staff-heading'>
+              Our Staff
+            </h2>
+          </div>
+          <div className="about-us-staff-intro-section-inner-container">
+            <div className="about-us-staff-intro-section-inner-container-left">
+              <div className="about-us-image-container">
+                <img src={staffPics[0]} alt="Chairman" />
+                <div className="about-us-image-container-text">
+                  <strong>Sheikh Abdullah Nazim</strong>
+                  <small>Founder/Chairman</small>
+                </div>
+              </div>
+            </div>
+            <div className="about-us-staff-intro-section-inner-container-right">
+              <p className='about-us-staff-content'>
+                Sheikh Abdullah Nazim graduated from Government College in 1974-1975.
+                His family owned a steel trading business but Mr. Abdullah had bigger plans.
+                Hence, he became the founder of the pioneer re-rolling mill in Islamabad in 1978.
+                Mr. Abdullah did not stop exploring new ventures and went on to own  Landmark Associates which is a real estate venture.
+                He is a celebrated local industrialist who held the positions of President of Islamabad Chamber of Commerce and Industry
+                in 1995, and later became the Chairman of Founder’s Group in 2008-2009 and again 2016-2017.
+                Chairman Pakistan steel re-rolling mills association
+              </p>
+            </div>
+          </div>
         </div>
-        <p className='about-us-staff-content'>
-          Sheikh Abdullah Nazim graduated from Government College in 1974-1975.
-          His family owned a steel trading business but Mr. Abdullah had bigger plans.
-          Hence, he became the founder of the pioneer re-rolling mill in Islamabad in 1978.
-          Mr. Abdullah did not stop exploring new ventures and went on to own  Landmark Associates which is a real estate venture.
-          He is a celebrated local industrialist who held the positions of President of Islamabad Chamber of Commerce and Industry
-          in 1995, and later became the Chairman of Founder’s Group in 2008-2009 and again 2016-2017.
-          Chairman Pakistan steel re-rolling mills association
-        </p>
+      
 
-        <h2 className='about-us-clients-heading'>
-          Our Clients
-        </h2>
-        <div className="about-us-image-container">
-          <img src={clientPics[0]} alt="comparte" style={{ width: "20rem" }} />
-          <strong style={{marginBottom: "4rem"}}>Compartee</strong>
-          {/* <small> Founder/Chairman</small> */}
+        <div className="about-us-clients-intro-section">
+          <h2 className='about-us-clients-heading'>
+            Our Clients
+          </h2>
+          <div className="about-us-image-container">
+            <img src={clientPics[0]} alt="comparte" style={{ width: "20rem" }} />
+            <strong style={{marginBottom: "4rem"}}>Compartee</strong>
+            {/* <small> Founder/Chairman</small> */}
+          </div>
+          <div className="about-us-image-container">
+            <img src={clientPics[1]} alt="Niblock" style={{ width: "20rem"}} />
+            <strong style={{marginBottom: "4rem"}}>Niblock</strong>
+            {/* <small> Founder/Chairman</small> */}
+          </div>
+          <div className="about-us-image-container">
+            <img src={clientPics[2]} alt="Steel Track" style={{ width: "20rem"}} />
+            <strong style={{marginBottom: "4rem"}}>Steel Track</strong>
+            {/* <small> Founder/Chairman</small> */}
+          </div>
         </div>
-        <div className="about-us-image-container">
-          <img src={clientPics[1]} alt="Niblock" style={{ width: "20rem"}} />
-          <strong style={{marginBottom: "4rem"}}>Niblock</strong>
-          {/* <small> Founder/Chairman</small> */}
-        </div>
-        <div className="about-us-image-container">
-          <img src={clientPics[2]} alt="Steel Track" style={{ width: "20rem"}} />
-          <strong style={{marginBottom: "4rem"}}>Steel Track</strong>
-          {/* <small> Founder/Chairman</small> */}
-        </div>
-
-
-
 
       </div>
       </Container>
